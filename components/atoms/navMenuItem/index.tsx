77cdb3b95c936051c84c472404de1d02202b45aa
Pierre Freire <pierrefreire.profis@gmail.com>
2024-04-29 06:22:01 -0300

    header style improvements

--- components/atoms/navMenuItem/index.tsx
+++ components/atoms/navMenuItem/index.tsx
@@ -5,7 +5,7 @@ interface NavMenuItem {
 
 export default function NavMenuItem({ content, href }: NavMenuItem) {
 	return (
-		<li className="py-2 px-4 flex items-center cursor-pointer hover:bg-[#ffffff20]">
+		<li className="py-4 px-6 flex items-center cursor-pointer hover:bg-[#ffffff20]">
 			<a href={href}>{content}</a>
 		</li>
 	);
--- components/molecules/navMenu/index.tsx
+++ components/molecules/navMenu/index.tsx
@@ -9,7 +9,7 @@ export default function NavMenu({ show }: NavMenuProps) {
 		<nav
 			className={`${
 				show ? "scale-y-1" : "scale-y-0"
-			} transition-transform duration-500 origin-top  w-full p-2 absolute top-full bg-slate-700 md:static md:w-auto md:scale-y-1`}
+			} transition-transform duration-500 origin-top  w-full absolute top-full bg-slate-700 md:static md:w-auto md:scale-y-1`}
 		>
 			<ul className="flex flex-col md:flex-row">
 				<NavMenuItem content="Skills" href="#skills" />
